3e6fca67ffd48ee21a1752038d8641ed29f32c5a
gustavoocz <serezinigustavo@gmail.com>
2024-11-14 23:49:36 -0300

    segundo commit

--- src/main/java/com/sesi/achadoseperdidos/config/WebConfig.java
+++ src/main/java/com/sesi/achadoseperdidos/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") 
-                .allowedOrigins("http://localhost:8081") 
+                .allowedOrigins("https://frontend-achados-e-perdidos.vercel.app") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE") 
                 .allowedHeaders("*");
     }
